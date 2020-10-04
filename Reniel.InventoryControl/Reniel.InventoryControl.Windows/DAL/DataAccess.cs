0a18c284e2d57be4c3dac6945c42c16dad6737a2
rmallaridavid18 <rmallridavid@csmbataan.ph.education>
2020-10-03 22:29:24 -0700

    All

---
+++ Reniel.InventoryControl/Reniel.InventoryControl.Windows/DAL/DataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reniel.InventoryControl.Windows.DAL
+{
+    public class DataAccess : DbContext
+    {
+        public DataAccess() : base("myConnectionString")
+        {
+            Database.SetInitializer(new Reniel.InventoryControl.Windows.DAL.DataInitializer());
+        }
+        public DbSet<Models.LoginUser> LoginUsers { get; set; }
+    }
+
+}
---
+++ Reniel.InventoryControl/Reniel.InventoryControl.Windows/DAL/DataInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reniel.InventoryControl.Windows.DAL
+{
+    public class DataInitializer : System.Data.Entity.DropCreateDatabaseAlways<DataAccess>
+    {
+        protected override void Seed(DataAccess context)
+        {
+            context.LoginUsers.Add(new Models.LoginUser()
+            {
+                Id = Guid.Parse("2ceee862-a549-4f31-9939-dbfc91ed2471"),
+                FirstName = "RenielKalboMasamangtao",
+                LastName = "David",
+                EmailAddress = "youjizz.com",
+                Status = Models.Enums.Status.Probationary,
+                Assignment = Models.Enums.Assignment.Field,
+
+
+
+
+            });
+
+
+        }
+
+    }
+}
