79120dee3ff4ea5c55cb9ccb647d7f7e90019af4
rmallaridavid18 <rmallridavid@csmbataan.ph.education>
2020-10-12 14:00:23 -0700

    BLL

---
+++ Reniel.InventoryControl/Reniel.InventoryControl.Windows/BBL/ProductBBL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reniel.InventoryControl.Windows.BLL
+{
+    public static class ProductBLL
+    {
+        public static DAL.ProductsDBContex db = new DAL.ProductsDBContex();
+        public static List<Models.Product> GetList(String sortBy = "ProductName", string sortOrder = "asc")
+
+
+        {
+            if (sortBy.ToLower() == "ProductName" && sortOrder.ToLower() == "asc")
+            {
+                return db.Products.OrderBy(e => e.ProductName).ToList();
+            }
+            else if (sortBy.ToLower() == "ProductName" && sortOrder.ToLower() == "asc")
+            {
+                return db.Products.OrderByDescending(e => e.ProductName).ToList();
+            }
+             else if (sortBy.ToLower() == "Price" && sortOrder.ToLower() == "asc")
+            {
+                return db.Products.OrderBy(e => e.ProductName).ToList();
+            }
+            else
+            {
+                return db.Products.OrderByDescending(e => e.ProductName).ToList();
+            }
+
+        }
+    }
+}
